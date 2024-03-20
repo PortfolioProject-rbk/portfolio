ba48eda9f30841fbaeef0f42b0f0268eda594c76
lindamnasri <mnasrilinda20@gmail.com>
2024-03-20 10:14:05 +0100

    created portfolio router

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,20 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const dotenv = require("dotenv");
+
 // Import configuration object
 const { APP_PORT } = dotenv.config().parsed;
 
 const sequelize = require("./database/index.js");
+
+//import modules router
+const portfolioRouter = require("./modules/portfolio/route.js");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/api/portfolio", portfolioRouter);
+app.use(cors());
 
 app.listen(APP_PORT, () => {
   console.log(`App listening on Post ${APP_PORT}`);
