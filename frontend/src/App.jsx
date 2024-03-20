e5ebe41f665d2614a3e44c3138946e268d680b7a
Gaddour <mannai.abdlkader@gmail.com>
2024-03-20 12:11:01 +0100

    establish wizard route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Link } from 'react-router-dom'
+import Wizard from './components/Index'
 function App() {
 
   return (
     <>
+    <Link to={'/wizard'}>Wizz</Link>
       <Routes>
         <Route path='/' element={<h1>Welcome</h1>} />
+        <Route path='/wizard/*' element={<Wizard />} />
       </Routes>
     </>
   )
---
+++ frontend/src/components/Index.jsx
@@ -0,0 +1,15 @@
+const Wizard = () => {
+    return (
+        <div className="grid grid-cols-[1fr_2fr]">
+            <div className="h-[300px] bg-slate-400">
+                
+            </div>
+            <div className="h-[300px] bg-slate-200">
+
+            </div>
+
+        </div>
+    )
+}
+
+export default Wizard
