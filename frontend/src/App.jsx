c7623d024e4d2b4f6be29d620b70b35f508f0181
aminelaarif <laarifamine5569@gmail.com>
2024-03-20 17:20:47 +0100

    created  route to craete an account

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import { Routes, Route } from 'react-router-dom'
+import Register from './login&register/Register'
 function App() {
 
   return (
     <>
       <Routes>
-        <Route path='/' element={<h1>Welcome</h1>} />
+        <Route path='/' element={<Register/>} />
       </Routes>
     </>
   )
