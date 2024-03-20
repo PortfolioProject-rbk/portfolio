1afff51763bdbae510a01de4a8b44beb4170c341
Gaddour <mannai.abdlkader@gmail.com>
2024-03-20 12:50:13 +0100

    fetch and display interests

--- frontend/src/components/wizard/Index.jsx
+++ frontend/src/components/wizard/Index.jsx
@@ -1,4 +1,26 @@
+import { useEffect, useState } from "react"
+import axios from 'axios'
+
 const Wizard = () => {
+
+    const [interests, setInterests] = useState([]);
+    const [selected, setSelected] = useState([]);
+
+    useEffect(() => {
+        fetchInterest();
+    }, [])
+
+    const fetchInterest = async () => {
+        try {
+            const { data } = await axios('http://127.0.0.1:3000/api/interest')
+            setInterests(data)
+        } catch (error) {
+            alert('Error fetching Interests ❌')
+            console.log('Error fetching Interests ❌\n', error);
+        }
+
+    }
+
     return (
         <div className="grid grid-cols-[1fr_2fr]">
 
@@ -6,8 +28,18 @@ const Wizard = () => {
             <div className="h-[300px] bg-slate-400">
 
             </div>
-            <div className="h-[300px] bg-slate-200">
-
+            <div className="h-[300px] bg-slate-200 p-3">
+                Selected Interests Box
+                <div className="px-3 bg-slate-50 py-3"></div>
+                Interests List Box
+                <div className="px-3 flex flex-wrap bg-slate-50 py-3">
+                    {interests.map((item, i) => (
+                        <div key={i} className="py-1 px-3 m-1 rounded-xl cursor-pointer bg-orange-200 hover:bg-orange-300">
+                            {item.name}
+                            <span className="font-[900]"> +</span>
+                        </div>
+                    ))}
+                </div>
             </div>
 
         </div>
