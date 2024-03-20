6306685f20d4662566a7e8cc794ac63c483c2919
selim021 <brahamselim09@gmail.com>
2024-03-20 16:25:44 +0100

    add route /search

--- backend/modules/portfolio/route.js
+++ backend/modules/portfolio/route.js
@@ -3,5 +3,5 @@ const portfolioController = require("./controller");
 
 router.post("/", portfolioController.create);
 router.get("/", portfolioController.getAll);
-
+router.post("/search",portfolioController.search)
 module.exports = router;
