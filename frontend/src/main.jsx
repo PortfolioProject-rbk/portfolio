31bc6e9eddf4c102087f9392150403d23f48a308
Gaddour <mannai.abdlkader@gmail.com>
2024-03-20 11:15:41 +0100

    init react router dom

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { HashRouter as Router } from 'react-router-dom';
 
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.min.js'
@@ -9,6 +10,8 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
 )
