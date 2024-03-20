83c3f7530d318286860c4b62ac2ec3db808776af
Gaddour <mannai.abdlkader@gmail.com>
2024-03-20 13:03:00 +0100

    Select Interests functionality

--- frontend/src/components/wizard/Index.jsx
+++ frontend/src/components/wizard/Index.jsx
@@ -4,7 +4,7 @@ import axios from 'axios'
 const Wizard = () => {
 
     const [interests, setInterests] = useState([]);
-    const [selected, setSelected] = useState([]);
+    const [selected, setSelected] = useState({});
 
     useEffect(() => {
         fetchInterest();
@@ -18,30 +18,47 @@ const Wizard = () => {
             alert('Error fetching Interests ❌')
             console.log('Error fetching Interests ❌\n', error);
         }
+    }
 
+    const selectInterest = (index) => {
+        setSelected({ ...selected, [index]: interests[index].name })
     }
 
     return (
         <div className="grid grid-cols-[1fr_2fr]">
 
             {/* // Todo: This will take the initial profile card with basic infos */}
-            <div className="h-[300px] bg-slate-400">
+            <div className="bg-slate-400">
 
             </div>
-            <div className="h-[300px] bg-slate-200 p-3">
+
+            <div className="bg-slate-200 p-3">
+
                 Selected Interests Box
-                <div className="px-3 bg-slate-50 py-3"></div>
-                Interests List Box
                 <div className="px-3 flex flex-wrap bg-slate-50 py-3">
-                    {interests.map((item, i) => (
-                        <div key={i} className="py-1 px-3 m-1 rounded-xl cursor-pointer bg-orange-200 hover:bg-orange-300">
-                            {item.name}
+                    {Object.entries(selected).map(([index, name]) => (
+                        <div key={index} className="py-1 px-3 m-1 rounded-xl cursor-pointer bg-orange-200 hover:bg-orange-300">
+                            {name}
                             <span className="font-[900]"> +</span>
                         </div>
                     ))}
                 </div>
-            </div>
 
+                Interests List Box
+                <div className="px-3 flex flex-wrap bg-slate-50 py-3">
+                    {interests.map((item, i) => (
+                        selected[i] ?
+                            <></> :
+                            <div
+                                onClick={() => (selectInterest(i))}
+                                key={i}
+                                className="py-1 px-3 m-1 rounded-xl cursor-pointer bg-orange-200 hover:bg-orange-300">
+                                {item.name}
+                                <span className="font-[900]"> +</span>
+                            </div>
+                    ))}
+                </div>
+            </div>
         </div>
     )
 }
