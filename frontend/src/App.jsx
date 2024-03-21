3b39ef0e0323b08d490d056c0518b43daa1133f6
aminelaarif <laarifamine5569@gmail.com>
2024-03-21 13:15:55 +0100

    path  for login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,11 @@
 
 import Register from './login&register/Register'
+import Login from './login&register/Login'
 import { Routes, Route, Link } from 'react-router-dom'
 import Wizard from './components/wizard/Index'
 function App() {
+
+  
 
   return (
     <>
@@ -10,10 +13,12 @@ function App() {
       <Link to={'/register'}>signup</Link>
       <Routes>
         <Route path='/register' element={<Register/>} />
+        <Route path='/login' element={<Login/>}/>
         <Route path='/' element={<h1>Welcome</h1>} />
         <Route path='/wizard/*' element={<Wizard />} />
       </Routes>
     </>
+
   )
 }
 
