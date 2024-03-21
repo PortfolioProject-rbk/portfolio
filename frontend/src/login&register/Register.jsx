0e847082a7bc90dc6569c7d3e28415b515365ea8
aminelaarif <laarifamine5569@gmail.com>
2024-03-21 13:17:27 +0100

    created register

--- frontend/src/login&register/Register.jsx
+++ frontend/src/login&register/Register.jsx
@@ -1,18 +1,42 @@
-import React from 'react'
+import axios from 'axios'
+import React,{useState} from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function Register() {
+
+  const[username,setUsername]=useState("")
+  const[password,setPassword]=useState("")
+
+
+ const navigate  = useNavigate()
+  
+
+  const create = (username,password)=>{
+    axios.post("http://localhost:3000/api/users/signup",{
+      username:username,
+      password:password
+    }).then((result)=>{
+      console.log(result)
+    }).catch((error)=>{
+      console.log(error)
+    })
+  }
   return (
+    
     <div>
-    <div className=' flex justify-center '>
-      <h1> create your account  </h1>
-      <h2><input className='form-control w-[300px] bg-[#a0a0a0] ' type="text" placeholder='username' /></h2>
-      <h2><input className='form-control w-[300px] bg-[#a0a0a0]' type="text" placeholder='password' /></h2>
-      <div>
-        <button>sign in </button>
-        <button>sign up </button>
+      <div className=' flex justify-center '>
+        <div  className='w-[400px]   '>
+
+          <h1> create your account  </h1>
+          <h2><input onChange={(e)=>setUsername(e.target.value)} className='form-control w-[300px] bg-[#a0a0a0] ' type="text" placeholder='username' /></h2>
+          <h2><input onChange={(e)=>setPassword(e.target.value)} className='form-control w-[300px] bg-[#a0a0a0]' type="text" placeholder='password' /></h2>
+          <div>
+            <button onClick={()=>{create(username,password)}} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full'>sign up </button>
+            <button   onClick={()=>{navigate("/login")}}  className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full'>sign in </button>
+          </div>
+        </div>
       </div>
     </div>
-</div>
   )
 }
 
