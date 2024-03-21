f2ed1dd2a9ee82d284ba4ac33e03c13b164a6e6a
Gaddour <mannai.abdlkader@gmail.com>
2024-03-21 12:43:24 +0100

    fix confict error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,19 +4,17 @@ import Wizard from "./components/wizard/Index";
 import Home from './components/wizard/Home';
 
 function App() {
+  return (
+    <>
+      <Link to="/profile">Profile </Link>
+      <Link to={"/wizard"}>Wizz</Link>
+      <Routes>
+        <Route path="/profile" element={<AddCardProfile />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/wizard/*' element={<Wizard />} />
+      </Routes>
+    </>
+  );
+}
 
-  function App() {
-    return (
-      <>
-        <Link to="/profile">Profile </Link>
-        <Link to={"/wizard"}>Wizz</Link>
-        <Routes>
-          <Route path="/profile" element={<AddCardProfile />} />
-          <Route path='/' element={<Home />} />
-          <Route path='/wizard/*' element={<Wizard />} />
-        </Routes>
-      </>
-    );
-  }
-
-  export default App;
+export default App;
