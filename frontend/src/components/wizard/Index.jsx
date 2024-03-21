496dd4477fb2347ed6d310e9c961b10716584c69
Gaddour <mannai.abdlkader@gmail.com>
2024-03-21 13:36:15 +0100

    add profile card

--- frontend/src/components/wizard/Index.jsx
+++ frontend/src/components/wizard/Index.jsx
@@ -56,6 +56,29 @@ const Wizard = () => {
 
             {/* // Todo: This will take the initial profile card with basic infos */}
             <div className="bg-slate-400">
+                <div className="flex justify-center mt-5">
+                    <div className="max-w-lg mx-auto my-10 bg-white rounded-lg shadow-md p-5">
+                        <div className="relative mx-auto w-36 rounded-full overflow-hidden">
+                            <img
+                                src="https://upload.wikimedia.org/wikipedia/fr/c/c4/Logo_Avatar_2.png"
+                                alt=""
+                            />
+                        </div>
+                        <h2 className="text-center text-2xl font-semibold mt-3 outline-none border-b-2 border-gray-300">
+                            {portfolio.fullName}
+                        </h2>
+                        <h3 className="text-center text-gray-600 mt-1 outline-none border-b-2 border-gray-300">
+                            {portfolio.profession}
+                        </h3>
+                        <p className="text-gray-600 mt-5 p-3 w-full border border-gray-300 rounded focus:outline-none focus:ring focus:border-blue-300">
+                            {portfolio.bio}
+                        </p>
+                        <ul className="mt-3 divide-y rounded bg-gray-100 py-2 px-3 text-gray-600 shadow-sm hover:text-gray-700 hover:shadow">
+                            <li className="flex items-center py-3 text-sm">{portfolio.email}</li>
+                            <li className="flex items-center py-3 text-sm">{portfolio.city}</li>
+                        </ul>
+                    </div>
+                </div>
             </div>
 
             <div className="bg-slate-200 p-3">
