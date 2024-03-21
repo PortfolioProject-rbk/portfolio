895401e417a9910dd5573a6861c0592e81cf2e36
aminelaarif <laarifamine5569@gmail.com>
2024-03-21 11:20:30 +0100

    small css changse

--- frontend/src/login&register/Register.jsx
+++ frontend/src/login&register/Register.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 function Register() {
   return (
     <div>
-    <div className='Card'>
+    <div className=' flex justify-center '>
       <h1> create your account  </h1>
-      <h2><input type="text" placeholder='username' /></h2>
-      <h2><input type="text" placeholder='password' /></h2>
+      <h2><input className='form-control w-[300px] bg-[#a0a0a0] ' type="text" placeholder='username' /></h2>
+      <h2><input className='form-control w-[300px] bg-[#a0a0a0]' type="text" placeholder='password' /></h2>
       <div>
         <button>sign in </button>
         <button>sign up </button>
