052f00325ac6052efa63743e61fc7b1ae72fd30a
Gaddour <mannai.abdlkader@gmail.com>
2024-03-22 11:06:21 +0100

    add wizard router

--- frontend/src/components/wizard/Index.jsx
+++ frontend/src/components/wizard/Index.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react"
 import axios from 'axios'
+import { Route, Routes, Link } from "react-router-dom";
+import AddCardProfile from "../portfolio/AddCardProfile";
 
 const Wizard = () => {
 
@@ -66,81 +68,103 @@ const Wizard = () => {
     }
 
     return (
-        <div className="grid grid-cols-[1fr_2fr]">
-
-            {/* // Todo: This will take the initial profile card with basic infos */}
-            <div className="bg-slate-400">
-                <div className="flex justify-center mt-5">
-                    <div className="max-w-lg mx-auto my-10 bg-white rounded-lg shadow-md p-5">
-                        <div className="relative mx-auto w-36 rounded-full overflow-hidden">
-                            <img
-                                src="https://upload.wikimedia.org/wikipedia/fr/c/c4/Logo_Avatar_2.png"
-                                alt=""
-                            />
-                        </div>
-                        <h2 className="text-center text-2xl font-semibold mt-3 outline-none border-b-2 border-gray-300">
-                            {portfolio.fullName}
-                        </h2>
-                        <h3 className="text-center text-gray-600 mt-1 outline-none border-b-2 border-gray-300">
-                            {portfolio.profession}
-                        </h3>
-                        <p className="text-gray-600 mt-5 p-3 w-full border border-gray-300 rounded focus:outline-none focus:ring focus:border-blue-300">
-                            {portfolio.bio}
-                        </p>
-                        <ul className="mt-3 divide-y rounded bg-gray-100 py-2 px-3 text-gray-600 shadow-sm hover:text-gray-700 hover:shadow">
-                            <li className="flex items-center py-3 text-sm">{portfolio.email}</li>
-                            <li className="flex items-center py-3 text-sm">{portfolio.city}</li>
-                        </ul>
+        <Routes>
+            <Route path="/" element={
+                <div className="flex justify-center bg-slate-400">
+                    <div className="p-2 px-5 bg-slate-100">
+                        <AddCardProfile />
+                        <Link className="btn btn-primary" to={'/wizard/inter'}>Next</Link>
                     </div>
                 </div>
-            </div>
+            } />
 
-            <div className="bg-slate-200 p-3">
+            <Route path="/*" element={
+                <div className="grid grid-cols-[1fr_2fr]">
 
-                <div>
-                    Selected Interests Box
-                </div>
-                {/* display all selected interests */}
-                <div className="px-3 flex flex-wrap bg-slate-50 py-3">
-                    {/* 
+                    {/* // Todo: This will take the initial profile card with basic infos */}
+                    <div className="bg-slate-400">
+                        <div className="flex justify-center mt-5">
+                            <div className="max-w-lg mx-auto my-10 bg-white rounded-lg shadow-md p-5">
+                                <div className="relative mx-auto w-36 rounded-full overflow-hidden">
+                                    <img
+                                        src="https://upload.wikimedia.org/wikipedia/fr/c/c4/Logo_Avatar_2.png"
+                                        alt=""
+                                    />
+                                </div>
+                                <h2 className="text-center text-2xl font-semibold mt-3 outline-none border-b-2 border-gray-300">
+                                    {portfolio.fullName}
+                                </h2>
+                                <h3 className="text-center text-gray-600 mt-1 outline-none border-b-2 border-gray-300">
+                                    {portfolio.profession}
+                                </h3>
+                                <p className="text-gray-600 mt-5 p-3 w-full border border-gray-300 rounded focus:outline-none focus:ring focus:border-blue-300">
+                                    {portfolio.bio}
+                                </p>
+                                <ul className="mt-3 divide-y rounded bg-gray-100 py-2 px-3 text-gray-600 shadow-sm hover:text-gray-700 hover:shadow">
+                                    <li className="flex items-center py-3 text-sm">{portfolio.email}</li>
+                                    <li className="flex items-center py-3 text-sm">{portfolio.city}</li>
+                                </ul>
+                                <div>
+                                    {portfolio.Interests ? portfolio.Interests.map(item => <div key={item.id} className="bg-[#e0e0e0]">{item.name}</div>) : ''}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <Routes>
+                        <Route path="/inter" element={
+                            <div className="bg-slate-200 p-3">
+
+                                <div>
+                                    Selected Interests Box
+                                </div>
+                                {/* display all selected interests */}
+                                <div className="px-3 flex flex-wrap bg-slate-50 py-3">
+                                    {/* 
                         since i have an array (selected) containing the index of the selected interests from the interests list/steate,
                         I can map on it and display item from the interest list of the corresponding index
                     */}
-                    {selected.map((index, i) => (
-                        <div
-                            onClick={() => (unselectInterest(i))}
-                            key={`selected${i}`}
-                            className="py-1 px-3 m-1 rounded-xl cursor-pointer bg-orange-200 hover:bg-orange-300">
-                            {interests[index].name}
-                            <span className="font-[900]"> +</span>
-                        </div>
-                    ))}
-                </div>
+                                    {selected.map((index, i) => (
+                                        <div
+                                            onClick={() => (unselectInterest(i))}
+                                            key={`selected${i}`}
+                                            className="py-1 px-3 m-1 rounded-xl cursor-pointer bg-orange-200 hover:bg-orange-300">
+                                            {interests[index].name}
+                                            <span className="font-[900]"> +</span>
+                                        </div>
+                                    ))}
+                                </div>
 
-                <div>
-                    Interests List Box
-                </div>
-                <div className="px-3 flex flex-wrap bg-slate-50 py-3">
-                    {interests.map((item, i) => (
-                        selected.includes(i) ?
-                            '' :
-                            <div
-                                onClick={() => (selectInterest(i))}
-                                key={`interests${i}`}
-                                className="py-1 px-3 m-1 rounded-xl cursor-pointer bg-orange-200 hover:bg-orange-300">
-                                {item.name}
-                                <span className="font-[900]"> +</span>
+                                <div>
+                                    Interests List Box
+                                </div>
+                                <div className="px-3 flex flex-wrap bg-slate-50 py-3">
+                                    {interests.map((item, i) => (
+                                        selected.includes(i) ?
+                                            '' :
+                                            <div
+                                                onClick={() => (selectInterest(i))}
+                                                key={`interests${i}`}
+                                                className="py-1 px-3 m-1 rounded-xl cursor-pointer bg-orange-200 hover:bg-orange-300">
+                                                {item.name}
+                                                <span className="font-[900]"> +</span>
+                                            </div>
+                                    ))}
+                                </div>
+                                <button
+                                    onClick={submitInterests}
+                                    className="btn btn-primary float-end ml-5">Submit</button>
+                                <Link
+                                    to={'/wizard/contacts'}
+                                    className="btn btn-primary float-end ml-5">Next</Link>
                             </div>
-                    ))}
+                        } />
+                    </Routes>
+
+
                 </div>
-                <button
-                    onClick={submitInterests}
-                    className="btn btn-primary float-end ml-5">Submit</button>
-                <button
-                    onClick={submitInterests}
-                    className="btn btn-primary float-end ml-5">Next</button>
-            </div>
-        </div>
+            } />
+
+        </Routes>
     )
 }
 
