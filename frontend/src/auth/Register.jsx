8c5c24642185ae56e5afa85d6a3da16621621052
aminelaarif <laarifamine5569@gmail.com>
2024-03-22 14:37:57 +0100

    restyling register

--- frontend/src/auth/Register.jsx
+++ frontend/src/auth/Register.jsx
@@ -7,6 +7,7 @@ function Register() {
   const[username,setUsername]=useState("")
   const[password,setPassword]=useState("")
 
+  console.log(username,password)
 
  const navigate  = useNavigate()
   
@@ -23,21 +24,69 @@ function Register() {
   }
   return (
     
-    <div>
-      <div className=' flex justify-center '>
-        <div  className='w-[400px]   '>
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+         
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+           Create account
+          </h2>
+        </div>
 
-          <h1> create your account  </h1>
-          <h2><input onChange={(e)=>setUsername(e.target.value)} className='form-control w-[300px] bg-[#a0a0a0] ' type="text" placeholder='username' /></h2>
-          <h2><input onChange={(e)=>setPassword(e.target.value)} className='form-control w-[300px] bg-[#a0a0a0]' type="text" placeholder='password' /></h2>
-          <div>
-            <button onClick={()=>{create(username,password)}} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full'>sign up </button>
-            <button   onClick={()=>{navigate("/login")}}  className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full'>sign in </button>
-          </div>
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form>
+            <div>
+              <label className="block text-sm font-medium leading-6 text-gray-900">
+                Username
+              </label>
+              <div className="mt-2">
+                <input
+                onChange={(e)=>setUsername(e.target.value)}
+                  
+                 
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label  className="block text-sm font-medium leading-6 text-gray-900">
+                  Password
+                </label>
+                <div className="text-sm">
+                  
+                </div>
+              </div>
+              <div className="mt-2">
+                <input
+                onChange={(e)=>setPassword(e.target.value)}
+                 
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+               onClick={()=>{create(username,password)}}
+                
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Sign up
+              </button>
+           
+            </div>
+          </form>
+
+       
         </div>
       </div>
-    </div>
   )
 }
-
+// onChange={(e)=>setUsername(e.target.value)}
+// onChange={(e)=>setPassword(e.target.value)}
+// onClick={()=>{create(username,password)}}
+// onClick={()=>{navigate("/login")}
 export default Register
+
+
