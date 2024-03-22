f1ec5f0fe61c03866f58bddf87b3d997e45637d8
lindamnasri <mnasrilinda20@gmail.com>
2024-03-22 12:37:28 +0100

    upload files and send to cloudinary

--- backend/index.js
+++ backend/index.js
@@ -1,32 +1,30 @@
 const express = require("express");
 const cors = require("cors");
 const cloudinary = require("cloudinary").v2;
-const app = express();
 const dotenv = require("dotenv");
-const userRouter = require("./modules/user/route.js")
+const multer = require("multer");
+
+const sequelize = require("./database/index.js");
+//import modules router
+const portfolioRouter = require("./modules/portfolio/route.js");
+const interestRouter = require("./modules/interest/route.js");
+const userRouter = require("./modules/user/route.js");
 
 // Import configuration object
 const { APP_PORT } = dotenv.config().parsed;
+
+// app.use(express.urlencoded({ extended: true }));
+const app = express();
+const upload = multer();
+
+app.use(upload.any());
 app.use(express.json());
-app.use(cors())
-// Import configuration object
-
-app.use("/api/users",userRouter)
-
-
-const sequelize = require("./database/index.js");
-
-//import modules router
-const portfolioRouter = require("./modules/portfolio/route.js");
-//import interest router
-const interestRouter = require("./modules/interest/route.js");
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+
 
 app.use("/api/portfolio", portfolioRouter);
 app.use("/api/interest", interestRouter);
+app.use("/api/users", userRouter);
 
 app.listen(APP_PORT, () => {
   console.log(`App listening on Post ${APP_PORT}`);
