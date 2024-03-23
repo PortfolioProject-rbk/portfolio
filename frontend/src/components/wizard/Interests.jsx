271a61c5dd96219959b72e0555c9661aeb26c0fc
Gaddour <mannai.abdlkader@gmail.com>
2024-03-23 19:15:12 +0100

    upgrade interest wizard

--- frontend/src/components/wizard/Interests.jsx
+++ frontend/src/components/wizard/Interests.jsx
@@ -5,7 +5,7 @@ import axios from 'axios'
 const Interests = ({ portfolio }) => {
 
     // This will contain the selected interests by the user
-    const [selected, setSelected] = useState([]);
+    const [selected, setSelected] = useState({});
     const [interests, setInterests] = useState([]);
 
     useEffect(() => {
@@ -26,7 +26,7 @@ const Interests = ({ portfolio }) => {
 
     // append the selected interest index from the interests array to the selected state
     const selectInterest = (index) => {
-        setSelected([...selected, index])
+        setSelected({ ...selected, [index]: 1 })
     }
 
     // splice the selected interest index from the selected state
@@ -34,8 +34,8 @@ const Interests = ({ portfolio }) => {
 
         // use the callback method so i won't have to create copy of the state
         // and I can deconstruct it from the parameter
-        setSelected(([...selected]) => {
-            selected.splice(index, 1)
+        setSelected(({ ...selected }) => {
+            delete selected[index]
             return selected
         })
     }
@@ -44,7 +44,7 @@ const Interests = ({ portfolio }) => {
     const submitInterests = async () => {
         try {
             // map the interest indexes to ids
-            const interestIds = selected.map(index => interests[index].id)
+            const interestIds = Object.keys(selected).map(index => interests[index].id)
             // send them to the endpoint to add them to the related portfolio
             await axios.post('http://127.0.0.1:3000/api/interest/user/' + portfolio.UserId, { interests: interestIds })
             setSelected([])
@@ -54,50 +54,36 @@ const Interests = ({ portfolio }) => {
     }
 
     return (
-        <div className="bg-slate-200 p-3">
+        <div className="wizard-addons">
 
-            <div>
-                Selected Interests Box
-            </div>
             {/* display all selected interests */}
-            <div className="px-3 flex flex-wrap bg-slate-50 py-3">
-                {/* 
-                        since i have an array (selected) containing the index of the selected interests from the interests list/steate,
-                        I can map on it and display item from the interest list of the corresponding index
-                    */}
-                {selected.map((index, i) => (
-                    <div
-                        onClick={() => (unselectInterest(i))}
-                        key={`selected${i}`}
-                        className="py-1 px-3 m-1 rounded-xl cursor-pointer bg-orange-200 hover:bg-orange-300">
-                        {interests[index].name}
-                        <span className="font-[900]"> +</span>
-                    </div>
-                ))}
-            </div>
-
-            <div>
-                Interests List Box
-            </div>
-            <div className="px-3 flex flex-wrap bg-slate-50 py-3">
-                {interests.map((item, i) => (
-                    selected.includes(i) ?
-                        '' :
+            <div className="wizard-form">
+                <div className="wizard-title">
+                    Select Your Interrests
+                </div>
+                <div className="px-3 flex flex-wrap bg-slate-50 py-3">
+                    {interests.map((item, i) => (
                         <div
-                            onClick={() => (selectInterest(i))}
+                            onClick={() => {
+                                if (selected[i]) unselectInterest(i)
+                                else selectInterest(i)
+                            }}
                             key={`interests${i}`}
-                            className="py-1 px-3 m-1 rounded-xl cursor-pointer bg-orange-200 hover:bg-orange-300">
+                            className={"interest-tag" + (selected[i] ? '-selected' : '')}>
                             {item.name}
-                            <span className="font-[900]"> +</span>
                         </div>
-                ))}
+                    ))}
+                </div>
             </div>
-            <button
-                onClick={submitInterests}
-                className="btn btn-primary float-end ml-5">Submit</button>
-            <Link
-                to={'/wizard/contacts'}
-                className="btn btn-primary float-end ml-5">Next</Link>
+            <div className="float-end">
+                <Link
+                    to={'/wizard'}
+                    className="btn btn-primary  ml-5">Prveious</Link>
+                <Link
+                    onClick={submitInterests}
+                    to={'/wizard/contacts'}
+                    className="btn btn-primary  ml-5">Next</Link>
+            </div>
         </div>
     )
 }
