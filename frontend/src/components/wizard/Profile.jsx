fbcf7c46c6789ecb4365aa95c4785e53afaa8c2f
Gaddour <mannai.abdlkader@gmail.com>
2024-03-24 10:39:38 +0100

    Add loader

--- frontend/src/components/wizard/Profile.jsx
+++ frontend/src/components/wizard/Profile.jsx
@@ -14,12 +14,13 @@ const ProfileForm = () => {
 
     const [image, setImage] = useState(imageHolder);
     const [bgImage, setBgImage] = useState();
-    
+
     const [loader, setLoader] = useState(false);
 
     const navigate = useNavigate()
 
     const createProfile = async () => {
+        setLoader(true)
         try {
             const formValues = {
                 fullName,
@@ -42,6 +43,7 @@ const ProfileForm = () => {
             console.log(result);
             navigate('/wizard/inter')
         } catch (error) {
+            setLoader(false)
             console.log(error);
         }
     };
@@ -73,89 +75,92 @@ const ProfileForm = () => {
 
     return (
         <div className="flex justify-center">
-            <div className="py-4 px-5 bg-white rounded border-2 shadow-md">
-                <div className="grid grid-cols-2 z-1 divide-x-2">
-                    <div className="max-w-md mx-auto my-10 p-6">
-                        <div className="mb-4">
-                            <div className="w-40 h-40 rounded-full shadow mx-auto overflow-hidden">
-                                {image && <img className="w-[100%] h-[100%] object-cover" src={image} alt="" />}
+            {loader ?
+                <div className="mt-[250px] spinner"></div> :
+                <div className="py-4 px-5 bg-white rounded border-2 shadow-md">
+                    <div className="grid grid-cols-2 z-1 divide-x-2">
+                        <div className="max-w-md mx-auto my-10 p-6">
+                            <div className="mb-4">
+                                <div className="w-40 h-40 rounded-full shadow mx-auto overflow-hidden">
+                                    {image && <img className="w-[100%] h-[100%] object-cover" src={image} alt="" />}
+                                </div>
+                                <input
+                                    className="form-control mt-3 mx-5 w-[70%]"
+                                    onChange={handlePhotoChange}
+                                    type="file"
+                                    accept="image/*"
+                                />
                             </div>
+
                             <input
-                                className="form-control mt-3 mx-5 w-[70%]"
-                                onChange={handlePhotoChange}
-                                type="file"
-                                accept="image/*"
+                                className="w-full px-3 py-2 mb-4 text-gray-700 border rounded"
+                                type="text"
+                                placeholder="Full Name"
+                                onChange={(event) => {
+                                    setFullName(event.target.value);
+                                }}
+                            />
+
+                            <input
+                                className="w-full px-3 py-2 mb-4 text-gray-700 border rounded"
+                                type="text"
+                                placeholder="Profession"
+                                onChange={(event) => {
+                                    setProfession(event.target.value);
+                                }}
                             />
                         </div>
-
-                        <input
-                            className="w-full px-3 py-2 mb-4 text-gray-700 border rounded"
-                            type="text"
-                            placeholder="Full Name"
-                            onChange={(event) => {
-                                setFullName(event.target.value);
-                            }}
-                        />
-
-                        <input
-                            className="w-full px-3 py-2 mb-4 text-gray-700 border rounded"
-                            type="text"
-                            placeholder="Profession"
-                            onChange={(event) => {
-                                setProfession(event.target.value);
-                            }}
-                        />
-                    </div>
-                    <div className="max-w-md mx-auto my-10 p-6">
-                        <textarea
-                            className="w-full h-24 px-3 py-2 mb-4 text-gray-700 border rounded"
-                            type="text"
-                            placeholder="Bio"
-                            onChange={(event) => {
-                                setBio(event.target.value);
-                            }}
-                        />
-
-                        <input
-                            type="text"
-                            placeholder="Email"
-                            onChange={(event) => {
-                                setEmail(event.target.value);
-                            }}
-                            className="w-full px-3 py-2 mb-4 text-gray-700 border rounded"
-                        />
-
-                        <input
-                            type="text"
-                            placeholder="City"
-                            onChange={(event) => {
-                                setCity(event.target.value);
-                            }}
-                            className="w-full px-3 py-2 mb-4 text-gray-700 border rounded"
-                        />
-
-                        <div className="flex justify-between">
-                            <label htmlFor="">Background Image</label>
-                            <input
-                                type="file"
-                                accept="image/*"
-                                onChange={handleBackgroundImageChange}
-                                className="px-3 py-2 mb-4 mr-2 text-gray-700 border rounded form-control"
+                        <div className="max-w-md mx-auto my-10 p-6">
+                            <textarea
+                                className="w-full h-24 px-3 py-2 mb-4 text-gray-700 border rounded"
+                                type="text"
+                                placeholder="Bio"
+                                onChange={(event) => {
+                                    setBio(event.target.value);
+                                }}
                             />
-                        </div>
 
+                            <input
+                                type="text"
+                                placeholder="Email"
+                                onChange={(event) => {
+                                    setEmail(event.target.value);
+                                }}
+                                className="w-full px-3 py-2 mb-4 text-gray-700 border rounded"
+                            />
+
+                            <input
+                                type="text"
+                                placeholder="City"
+                                onChange={(event) => {
+                                    setCity(event.target.value);
+                                }}
+                                className="w-full px-3 py-2 mb-4 text-gray-700 border rounded"
+                            />
+
+                            <div className="flex justify-between">
+                                <label htmlFor="">Background Image</label>
+                                <input
+                                    type="file"
+                                    accept="image/*"
+                                    onChange={handleBackgroundImageChange}
+                                    className="px-3 py-2 mb-4 mr-2 text-gray-700 border rounded form-control"
+                                />
+                            </div>
+
+                        </div>
+                    </div>
+                    <div className="float-end">
+
+                        <button
+                            className="primary-button"
+                            onClick={createProfile}
+                        >
+                            Next
+                        </button>
                     </div>
                 </div>
-                <div className="float-end">
-
-                    <button
-                        className="primary-button"
-                        onClick={createProfile}
-                    >
-                        Next
-                    </button>
-                </div>
-            </div>
+            }
         </div>
     );
 };
