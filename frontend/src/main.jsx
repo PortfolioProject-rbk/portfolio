dd3f50a105f834ca7bdb92f8806ee6768e9ae0a9
Gaddour <mannai.abdlkader@gmail.com>
2024-03-24 12:57:04 +0100

    HashRouter -> BrowserRouter

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { HashRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.min.js'
