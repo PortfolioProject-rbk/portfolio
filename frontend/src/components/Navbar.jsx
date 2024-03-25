3c1923df7412a789f15672fd2bf1cbae4ddcd22a
Gaddour <mannai.abdlkader@gmail.com>
2024-03-25 20:04:19 +0100

    edit navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,18 +1,14 @@
 import { useNavigate } from "react-router-dom"
- import React, {useState} from "react"
+import React, { useState } from "react"
 const Navbar = () => {
-    
+
 
     const [hover, setHover] = useState(false)
     const navigate = useNavigate()
 
     const navItems = [
+        { id: 0, title: 'Home', path: "/" },
         { id: 1, title: '👤 Profile', path: "/profile" },
-        { id: 2, title: 'Wizz', path: "/wizard" },
-        { id: 3, title: 'Card', path: "/Card" },
-        { id: 4, title: 'signup', path: "/register" },
-        { id: 5, title: 'Edit', path: "/edit" },
-        { id: 5 , title:'login', path:"/login" }
     ]
 
     return (
@@ -31,13 +27,12 @@ const Navbar = () => {
                 ))}
             </div>
             <div onMouseEnter={() => setHover(true)}
-                onMouseLeave={() => setHover(false)}   
-            className="mx-3 w-[50px] h-[50px] rounded-full bg-black">
-                {hover ? <button onClick={()=>
-                {
+                onMouseLeave={() => setHover(false)}
+                className="mx-3 w-[50px] h-[50px] rounded-full bg-black">
+                {hover ? <button onClick={() => {
                     localStorage.removeItem("token")
                     location.reload()
-            }} className="w-[200px] h-[100px] bg-gray-700 absolute right-0 top-[60px]" >logout</button> : "" }
+                }} className="w-[200px] h-[100px] bg-gray-700 absolute right-0 top-[60px]" >logout</button> : ""}
             </div>
         </nav>
     )
