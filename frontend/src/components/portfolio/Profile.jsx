a03bcb570ee455a628e46c30397d82fa88ca076b
Gaddour <mannai.abdlkader@gmail.com>
2024-03-25 18:18:15 +0100

    Use Profile component inside of wizard

--- frontend/src/components/portfolio/Profile.jsx
+++ frontend/src/components/portfolio/Profile.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 
 import '../../assets/style/particles.scss'
 
-const Profile = () => {
+const Profile = ({ small }) => {
 
     const particles = new Array(100).fill(null);
 
@@ -27,13 +27,14 @@ const Profile = () => {
     return (
         <div
             className="flex justify-center mt-2 mx-auto font-[Overpass]">
-            <div id="particle-container">
+
+            {!small && <div id="particle-container">
                 {particles.map((_, index) => (
                     <div key={index} className="particle"></div>
                 ))}
-            </div>
+            </div>}
             {portfolio ?
-                <div className="w-[700px] mx-auto my-3 rounded border-[1px] border-[#909090] overflow-hidden shadow-md bg-white">
+                <div className={`${small ? 'w-[450px]' : 'w-[700px]'} mx-auto my-3 rounded border-[1px] border-[#909090] overflow-hidden shadow-md bg-white`}>
                     <div
                         style={{ backgroundImage: `url(${portfolio.backgroundImage})`, backgroundSize: 'cover' }}
                         className="flex justify-center items-center bg-gray-100">
@@ -47,7 +48,7 @@ const Profile = () => {
                         <h2 className="text-center text-2xl mt-3 text-orange-400 font-bold">{portfolio.fullName}</h2>
                         <h3 className="text-center text-gray-600 mt-1 font-medium">{portfolio.profession}</h3>
                         {/* <p className="text-gray-600 mt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed aliquet, lacus a euismod tincidunt, dui est efficitur quam, in venenatis ligula ex id urna. Nunc nec eleifend libero. Nam tincidunt dui id justo aliquet, id dictum urna laoreet. Nam nec ante et nulla egestas posuere ut ac risus. Nullam gravida ipsum vel tristique feugiat. Nam sed fermentum ligula. Vivamus fermentum, nulla et sodales condimentum, elit arcu pharetra magna, nec vehicula mi lorem et ex.</p> */}
-                        <div className="grid grid-cols-2 mt-2 divide-x">
+                        <div className={`grid ${small ? '' : 'grid-cols-2'} mt-2 divide-x`}>
                             <div className="mt-4 px-3 text-[18px]">
                                 <ul className="divide-y">
                                     <div className="flex items-center">
@@ -69,12 +70,12 @@ const Profile = () => {
                                 ))}
                             </div>
                         </div>
-                        <div className="grid gap-1 grid-cols-2 mt-2 divide-x">
+                        <div className={`grid gap-1 ${small ? '' : 'grid-cols-2'} mt-2 divide-x`}>
 
                             <p className="text-gray-600 p-3 w-full border border-gray-300 rounded focus:outline-none focus:ring focus:border-blue-300">
                                 {portfolio.bio}
                             </p>
-                            <div className="user-socials py-3">
+                            {!small && <div className="user-socials py-3">
                                 {portfolio.Contacts && portfolio.Contacts.map(item => (
                                     <div key={item.id} className="user-social">
                                         <img src={`http://127.0.0.1:3000/socials/${item.icon}`} alt="" />
@@ -82,7 +83,7 @@ const Profile = () => {
                                     </div>
                                 )
                                 )}
-                            </div>
+                            </div>}
                         </div>
                     </div>
                 </div> : ''
--- frontend/src/components/wizard/Index.jsx
+++ frontend/src/components/wizard/Index.jsx
@@ -5,6 +5,7 @@ import Interests from "./Interests";
 import Contacts from "./Contacts";
 import ProfileForm from "./Profile";
 import Spinner from "../Spinner";
+import Profile from "../portfolio/Profile";
 
 const Wizard = () => {
 
@@ -38,33 +39,8 @@ const Wizard = () => {
                         <div className="wizard-grid">
 
                             {/* // Todo: This will take the initial profile card with basic infos */}
-                            <div className="px-2 border-2 border-[black]">
-                                <div className="flex justify-center mt-5">
-                                    <div className="max-w-lg mx-auto my-10 bg-white rounded-lg shadow-md p-5">
-                                        <div className="relative mx-auto w-36 rounded-full overflow-hidden">
-                                            <img
-                                                src="https://upload.wikimedia.org/wikipedia/fr/c/c4/Logo_Avatar_2.png"
-                                                alt=""
-                                            />
-                                        </div>
-                                        <h2 className="text-center text-2xl font-semibold mt-3 outline-none border-b-2 border-gray-300">
-                                            {portfolio.fullName}
-                                        </h2>
-                                        <h3 className="text-center text-gray-600 mt-1 outline-none border-b-2 border-gray-300">
-                                            {portfolio.profession}
-                                        </h3>
-                                        <p className="text-gray-600 mt-5 p-3 w-full border border-gray-300 rounded focus:outline-none focus:ring focus:border-blue-300">
-                                            {portfolio.bio}
-                                        </p>
-                                        <ul className="mt-3 divide-y rounded bg-gray-100 py-2 px-3 text-gray-600 shadow-sm hover:text-gray-700 hover:shadow">
-                                            <li className="flex items-center py-3 text-sm">{portfolio.email}</li>
-                                            <li className="flex items-center py-3 text-sm">{portfolio.city}</li>
-                                        </ul>
-                                        <div>
-                                            {portfolio.Interests ? portfolio.Interests.map(item => <div key={'interest' + item.id} className="bg-[#e0e0e0]">{item.name}</div>) : ''}
-                                        </div>
-                                    </div>
-                                </div>
+                            <div className="px-2 border-0">
+                                <Profile small={true} />
                             </div>
                             <Routes>
                                 <Route path="/inter" element={<Interests portfolio={portfolio} />} />
