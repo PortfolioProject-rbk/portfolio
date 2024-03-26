012ba395cafa2cf72ea0d5e4cd7d350413326815
aminelaarif <laarifamine5569@gmail.com>
2024-03-26 12:42:26 +0100

    redesigned sign up

--- frontend/src/auth/Register.jsx
+++ frontend/src/auth/Register.jsx
@@ -5,24 +5,25 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import Paper from '@mui/material/Paper';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
-
+import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 
 
 
-// TODO remove, this demo shouldn't need to reset the theme.
+
+
 
 const defaultTheme = createTheme();
 
 export default function SignUp() {
-  
-   
   const[username,setUsername]=useState("")
   const[password,setPassword]=useState("")
 
@@ -43,29 +44,15 @@ export default function SignUp() {
     })
   }
 
+  ;
+
   return (
     <ThemeProvider theme={defaultTheme}>
-    <Grid container component="main" sx={{ height: '100vh' }}>
-      <CssBaseline />
-      <Grid
-        item
-        xs={false}
-        sm={4}
-        md={7}
-        sx={{
-          backgroundImage: 'url(https://images-ext-1.discordapp.net/external/xW3YsAffsq79kgwgIfNuKRV8BFOi_7uF4h3LDuwzS2o/%3Fsize%3D626%26ext%3Djpg%26ga%3DGA1.1.1807355941.1705010241%26semt%3Dais/https/img.freepik.com/free-vector/polygon-lines-background_1035-7063.jpg?format=webp&width=660&height=662)',
-          backgroundRepeat: 'no-repeat',
-          backgroundColor: (t) =>
-            t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-        }}
-      />
-      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
         <Box
           sx={{
-            my: 8,
-            mx: 4,
+            marginTop: 8,
             display: 'flex',
             flexDirection: 'column',
             alignItems: 'center',
@@ -75,53 +62,60 @@ export default function SignUp() {
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
-            Sign Up
+            Sign up
           </Typography>
-          <Box component="form" noValidate sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              onChange={(e) => setUsername(e.target.value)}
-              label="username"
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              onChange={(e) => setPassword(e.target.value)}
-
-            />
-
+         
+            <Grid container spacing={2}>
+             
+             
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="User Name "
+                  onChange={(e)=>setUsername(e.target.value)}
+                 
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                  onChange={(e)=>setPassword(e.target.value)}
+                />
+              </Grid>
+           
+            </Grid>
             <Button
               type="button"
               fullWidth
               variant="contained"
+              onClick={()=>{
+                create(username,password)}
+                
+              }
               sx={{ mt: 3, mb: 2 }}
-              onClick={() => { create(username, password) }}
             >
               Sign Up
             </Button>
-            <Grid container>
-              <Grid item xs>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
                 
-              </Grid>
-              <Grid  item>
+                <h3 className='hover' onClick={()=>{navigate("/login")}} >Already have an account? Sign in</h3>  
                 
-                    <h3 className='hover'  onClick={()=>navigate("/login")}>Already have an account? Sign in</h3> 
-               
               </Grid>
             </Grid>
-            
           </Box>
-        </Box>
-      </Grid>
-    </Grid>
-  </ThemeProvider>
-);
-}
+        
+        
+      </Container>
+    </ThemeProvider>
+  );
+        }
+      
